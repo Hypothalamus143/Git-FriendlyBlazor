b536e64ed4a54f9b942a76ceb6d7fc4f8d146e1f
LAB23_USER ON WS03 <L23Y16W03@LabHCI.net>
2026-09-17 16:23:27 +0800

    Initial Commit

---
+++ Models/UserRegisterModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazorApp.Models;
+
+public class UserRegisterModel
+{
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Confirm Password is required")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
